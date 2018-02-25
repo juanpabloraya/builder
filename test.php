7ee12229998c8ec7f84e2417b9f2d94fd18cb9ab
pablex <juanpabloraya@gmail.com>
2018-02-24 23:35:19 -0400

    Implementacion del patron builder

---
+++ test.php
@@ -0,0 +1,9 @@
+<html>
+<head><title>Testing the HTMLPage</title></head>
+<body>
+<h1>Testing the HTMLPage</h1>
+Testing, testing, testing!
+Testing, testing, testing, or!
+Testing, testing, testing, more!
+</body>
+</html>
